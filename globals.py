a529b19e48e902598685ab4953c7358080fb7e41
usama13o <osama.zadan@gmail.com>
2021-03-31 13:53:59 +0000

    forgot to add gloabls.py

---
+++ globals.py
@@ -0,0 +1,10 @@
+####GLOBALS####
+#num of filtered slides to generate 
+NUM_FILTERED=1
+#which slide of the filtered to process
+PROCESS_SLIDE='any'
+#where are the slides to be filtered. format --> '/PATH/to/slides/' 
+SLIDES_PATH='/content/ihc/'
+#pred for tumour {1} or normal {2} cells
+TARG_PRED=1
+####GLOBALS####
